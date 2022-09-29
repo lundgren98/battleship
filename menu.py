b3f34b0409f2bc33f1d7d54a327bd8ae38e993ca
lundgren98 <a.k.o.lundgren@gmail.com>
2022-09-29 10:21:10 +0200

    Saving ship placement and player loading from file

--- menu.py
+++ menu.py
@@ -4,6 +4,20 @@ from time import sleep
 import os
 import csv
 
+def yes_no_input(message, yes_is_default = True):
+    if yes_is_default:
+        choices = 'Y/n'
+        short = 'n'
+        verbose = 'no'
+    else:
+        choices = 'y/N'
+        short = 'y'
+        verbose = 'yes'
+    u_input = input(f'{message} [{choices}]\n> ').lower()
+    if u_input == short or u_input == verbose:
+        return not yes_is_default
+    return yes_is_default
+
 def print_boards(top_board, bottom_board):
     os.system('clear')
     print(top_board)
@@ -94,21 +108,33 @@ def manual_placement(player_board, enemy_board):
 
 def place_from_file(path, board):
     board_state = []
-    with open(path, 'r') as f:
-        r = csv.reader(f)
-        for row in r:
-            board_state.append([int(x) for x in row])
-    board.board = board_state
+    try:
+        with open(path, 'r') as f:
+            r = csv.reader(f)
+            for row in r:
+                board_state.append([int(x) for x in row])
+    except FileNotFoundError:
+        print(f'Could not find file {path}')
+    except ValueError:
+        print(f'Invalid file {path}')
+    else:
+        board.board = board_state
+        return True
+    return False
 
 def place_phase(player_board, enemy_board):
-    #manual_placement(player_board, enemy_board)
-    place_from_file('ai_ships.csv', player_board)
+    if yes_no_input('Do you want to load ship placement from file?'):
+        if not place_from_file(input('file to read: '), player_board):
+            return True
+    else:
+        manual_placement(player_board, enemy_board)
     place_from_file('ai_ships.csv', enemy_board)
+    return False
 
 def bomb_phase(player_board, enemy_board):
     player_turn = True 
     not_hit_pl = [(x,y) for x in range(10) for y in range(10)]
-    #not_hit_en = [(x,y) for x in range(10) for y in range(10)]
+    not_hit_en = [(x,y) for x in range(10) for y in range(10)]
     err_msg = None
     while player_board.health() and enemy_board.health():
         print_boards(player_board, enemy_board)
@@ -130,13 +156,7 @@ def bomb_phase(player_board, enemy_board):
     print_boards(player_board, enemy_board)
     return player_board.health()
 
-def main():
-    # ask for confirmation
-    player_board = Board()
-    enemy_board = Board(hide = True)
-
-    place_phase(player_board, enemy_board)
-    bomb_phase(player_board, enemy_board)
+def print_winner_message(player_board, enemy_board):
     if player_board.health():
         win_or_lose = 'WON'
         winner = 'you'
@@ -147,6 +167,45 @@ def main():
         loser_board = player_board
     print(f'YOU {win_or_lose}! It took {winner} '
           f'{loser_board.shots_fired()} tries.')
+
+def save_stats(path, player_board, enemy_board):
+    if player_board.health():
+        winner = 'you'
+        tries = enemy_board.shots_fired()
+    else:
+        winner = 'enemy'
+        tries = player_board.shots_fired()
+    with open(path, 'a') as f:
+        f.write(f'{winner},{tries}\n')
+
+def save_ship_placement(board):
+    if not yes_no_input('Do you want to save your ship placements?'):
+        return False
+    path = input('file to write: ')
+    if os.path.exists(path):
+        if not yes_no_input(
+                f'The file {path} already exists.\n'
+                'Are you sure you want to overwrite it?', False):
+            return True
+    ships = [[v & Board.SHIP for v in row] for row in board.board]
+    write_board_to_file(path, ships)
+    return False
+
+def write_board_to_file(path, board):
+    with open(path, 'w') as f:
+        for row in board:
+            f.write(','.join(map(str, row)) + '\n')
+
+def main():
+    player_board = Board()
+    enemy_board = Board(hide = True)
+    while place_phase(player_board, enemy_board):
+        pass
+    bomb_phase(player_board, enemy_board)
+    print_winner_message(player_board, enemy_board)
+    save_stats('stats.csv', player_board, enemy_board)
+    while save_ship_placement(player_board):
+        pass
 
 
 if __name__ == '__main__':
